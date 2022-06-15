9bc53e502bf331f0af64d00e67d19935ff76c659
GuilhermeBarbosaDev <guilhermebarbosajob@gmail.com>
2022-06-15 16:14:18 -0300

    Basic delete from db

--- src/main/java/br/com/springboot/controllers/UserController.java
+++ src/main/java/br/com/springboot/controllers/UserController.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,5 +47,11 @@ private List<User> users = new ArrayList<User>();
     public List<User> list(){
 
         return users;
+    }
+
+    @DeleteMapping("/deleteUser/{id}")
+    public Boolean deleteOrder(@PathVariable(value = "id") Long id) {
+         // Access the DB and delete the order
+         return true;
     }
 }
